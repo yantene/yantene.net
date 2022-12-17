168699470a8b3eadcc15bef2b3653953b60b7fb8
yantene <contact@yantene.net>
2022-12-17 16:33:20 +0000

    feat(api): create openapi generation script

--- backend/src/app/openapi.ts
+++ backend/src/app/openapi.ts
@@ -1,10 +1,18 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from "@nestjs/swagger";
 
+export function title(): string {
+  return process.env.npm_package_name ?? "api";
+}
+
+export function version(): string {
+  return process.env.npm_package_version ?? "unknown";
+}
+
 export function generateOpenAPI(app: INestApplication): OpenAPIObject {
   const config = new DocumentBuilder()
-    .setTitle(process.env.npm_package_name ?? "backend")
-    .setVersion(process.env.npm_package_version ?? "unknown")
+    .setTitle(title())
+    .setVersion(version())
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
