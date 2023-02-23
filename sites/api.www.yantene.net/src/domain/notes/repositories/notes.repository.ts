d29ec5c215da0de628bbae18a0642f6cf964adb6
yantene <contact@yantene.net>
2023-02-23 02:07:37 +0000

    feat(api.www.yantene.net): implement NotesRepository#findByTitle function

--- sites/api.www.yantene.net/src/domain/notes/repositories/notes.repository.ts
+++ sites/api.www.yantene.net/src/domain/notes/repositories/notes.repository.ts
@@ -1,10 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+import { toTemporalInstant } from "@js-temporal/polyfill";
 import { Note } from "../models/note.entity";
 import { NotesRepositoryInterface } from "../models/notes.repository.interface";
 import { Title } from "../models/title.value-object";
+import { Body } from "../models/body.value-object";
 
 export class NotesRepository implements NotesRepositoryInterface {
-  findByTitle(_title: Title): Promise<Note | undefined> {
-    // TODO: implement me!
-    return (async (): Promise<undefined> => undefined)();
+  #prisma: PrismaClient;
+
+  constructor() {
+    this.#prisma = new PrismaClient();
+  }
+
+  async findByTitle(title: Title): Promise<Note | undefined> {
+    const foundNote = await this.#prisma.note.findUnique({
+      where: { title: title.value },
+    });
+
+    if (foundNote == null) {
+      return undefined;
+    }
+
+    const note = new Note(
+      new Title(foundNote.title),
+      toTemporalInstant.bind(foundNote.createdAt)(),
+      toTemporalInstant.bind(foundNote.modifiedAt)(),
+      new Body(foundNote.body),
+    );
+
+    return note;
   }
 }
