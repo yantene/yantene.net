c20b1733128f45c7790546a469ae982bd02314da
yantene <contact@yantene.net>
2023-02-23 16:07:31 +0000

    test(api.www.yantene.net): create tests for NotesRepository#findByTitle()

--- sites/api.www.yantene.net/src/domain/notes/repositories/notes.repository.spec.ts
+++ sites/api.www.yantene.net/src/domain/notes/repositories/notes.repository.spec.ts
@@ -1,7 +1,15 @@
+import { Temporal } from "@js-temporal/polyfill";
 import { Test, TestingModule } from "@nestjs/testing";
+import { PrismaClient } from "@prisma/client";
+import { Body } from "../models/body.value-object";
+import { Title } from "../models/title.value-object";
 import { NotesRepository } from "./notes.repository";
 
-describe("notesRepository", () => {
+jest.mock("@prisma/client", () => ({
+  PrismaClient: jest.fn().mockImplementation(() => jestPrisma.client),
+}));
+
+describe("NotesRepository", () => {
   let repository: NotesRepository;
 
   beforeEach(async () => {
@@ -14,5 +22,36 @@ describe("notesRepository", () => {
 
   it("should be defined", () => {
     expect(repository).toBeDefined();
+  });
+
+  describe("#findByTitle()", () => {
+    it("returns undefined if a nonexistent title is given", async () => {
+      const note = await repository.findByTitle(
+        new Title("存在しないタイトル"),
+      );
+
+      expect(note).toBeUndefined();
+    });
+
+    it("returns a note if the note title is given", async () => {
+      const title = new Title("存在するタイトル");
+      const now = Temporal.Now.instant();
+      const body = new Body("記事の内容");
+
+      const prisma = new PrismaClient();
+
+      await prisma.note.create({
+        data: {
+          title: title.value,
+          createdAt: new Date(now.epochMilliseconds),
+          modifiedAt: new Date(now.epochMilliseconds),
+          body: body.value,
+        },
+      });
+
+      const note = await repository.findByTitle(title);
+
+      expect(note?.title?.value).toBe(title.value);
+    });
   });
 });
