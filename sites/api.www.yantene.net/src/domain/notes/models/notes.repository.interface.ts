d5467886a8997c71c951e832dbb0bf71d3fc5a44
Shuhei YOSHIDA <contact@yantene.net>
2023-04-21 16:53:10 +0000

    feat(api.www.yantene.net): reexamine NotesRepositoryInterface

--- sites/api.www.yantene.net/src/domain/notes/models/notes.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Note } from "./note.entity";
-import { Title } from "./title.value-object";
-
-export type NotesRepositoryInterface = {
-  findByTitle(title: Title): Promise<Note | undefined>;
-};
