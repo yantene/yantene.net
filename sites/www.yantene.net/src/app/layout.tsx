0b234570e4c1dc9d8d4a643e72753ac0afabd15a
Shuhei YOSHIDA <contact@yantene.net>
2023-10-01 06:41:15 +0000

    fix(www.yantene.net): metadata was not applied due to not being exported

--- sites/www.yantene.net/src/app/layout.tsx
+++ sites/www.yantene.net/src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import Header from "../components/shared/Header";
 import Menu from "../components/shared/Menu";
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "やんてね！",
   description:
     "やんてねの本名や年齢は？彼女はいるの？メンバーとの不仲説は本当？それ以外のすべてを掲載！",
