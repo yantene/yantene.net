f8082fbea2a31e088a722e06d202846097064a11
Shuhei YOSHIDA <contact@yantene.net>
2023-10-03 17:47:55 +0000

    build(www.yantene.net): create Dockerfile for production

--- sites/www.yantene.net/next.config.mjs
+++ sites/www.yantene.net/next.config.mjs
@@ -2,10 +2,13 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
-const withVanillaExtract = createVanillaExtractPlugin();
+const nextConfig = {
+  output: "standalone",
+};
 
-const withPWA = withPWAInit({
-  dest: "public",
-});
-
-export default withVanillaExtract(withPWA());
+export default [
+  createVanillaExtractPlugin(),
+  withPWAInit({
+    dest: "public",
+  }),
+].reduce((config, plugin) => plugin(config), nextConfig);
